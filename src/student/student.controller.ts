939aec839264d32f0a78f532b459875fe71eca9c
mdselimme <mdselimdev@gmail.com>
2025-11-04 23:25:05 +0600

    add: student create update get getbyid api

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { StudentService } from './student.service';
+import { Student } from './student.schema';
 
 @Controller('student')
-export class StudentController {}
+export class StudentController {
+    constructor(private readonly studentService: StudentService) { };
+
+    @Get()
+    async getAllStudent() {
+        return this.studentService.getAllStudent();
+    }
+
+    @Get(':id')
+    async getStudentById(@Param('id') id: string) {
+        return this.studentService.getStudentById(id);
+    }
+
+    @Post()
+    async addStudent(@Body() data: Partial<Student>) {
+        return this.studentService.createStudent(data);
+    }
+
+    @Patch(':id')
+    async updateStudentById(@Body() data: Partial<Student>, @Param('id') id: string) {
+        return this.studentService.updateStudentById(id, data);
+    }
+}
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Student, StudentDocument } from './student.schema';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+    constructor(@InjectModel(Student.name) private studentModel: Model<StudentDocument>) { }
+
+    async createStudent(data: Partial<Student>): Promise<Student> {
+        const newStudent = new this.studentModel(data);
+        return newStudent.save();
+    };
+
+    async getAllStudent(): Promise<Student[]> {
+        const allStudent = this.studentModel.find().exec();
+        return allStudent;
+    };
+
+    async getStudentById(id: string): Promise<Student | string> {
+        const existingStudent = await this.studentModel.findById(id).exec();
+        if (!existingStudent) {
+            throw new NotFoundException(`Student #${id} not found`);
+        }
+        return existingStudent;
+    };
+
+    async updateStudentById(id: string, data: Partial<Student>): Promise<Student> {
+        const updateStudent = await this.studentModel.findByIdAndUpdate(id, data, { new: true }).exec();
+        if (!updateStudent) {
+            throw new NotFoundException(`Student #${id} not found`);
+        }
+        return updateStudent;
+    }
+}
